2ce1c849f10361347a937f8e4900269ea613c10e
wanyanyan <wanyanya@126.com>
2017-01-05 10:28:43 +0800

    修复图例显示出错的问题

--- static/svgedit/editor/extensions/ext-legend.js
+++ static/svgedit/editor/extensions/ext-legend.js
@@ -86,6 +86,11 @@ svgEditor.addExtension('ext-legend', function() {
 			if(legendArr[i].type === "symbol"){
 				var iconUrl = prefix+legendArr[i].styles[0]['icon-image']+"?access_token="+options.access_token;
 				var res = $.ajax({url:iconUrl,async:false});
+				if(res.statusText!=="OK"){
+					legendArr.splice(i,1);
+					i = i-1;
+					continue;
+				}
 				var svgStr = res.responseText;
 				var newDoc = $.parseXML(svgStr);
 				// set new svg document
